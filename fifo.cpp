adee9bcad06d496cdf3f0335b972d1cc8e04115d
kysis666 <50187331+kysis666@users.noreply.github.com>
2020-05-25 16:21:04 +0200

    Add files via upload

---
+++ fifo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+using namespace std;
+ 
+int main(int argc, char** argv) {
+	
+	cout << "Content-type: text/html\n\n<title>CGI FIFO</title>";
+               
+    queue <string> kolejkaLiczb;
+        string get;
+        fstream plik;
+
+	plik.open("tekst.txt");  
+	if(plik.good())  
+		while(!plik.eof())  
+    	{
+        	getline(plik,get);
+            kolejkaLiczb.push( get );
+
+		    cout << "\n[{"<<get<<"}]\n";
+        }
+    plik.close();  
+
+ return 0;
+}
